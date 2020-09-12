273215e4bb5e8b98917a61aac4d709949cb7857c
Artem Khismatulln <a.khismatulli@gmail.com>
2020-09-12 15:52:55 +0500

    Исправил 7 задание

--- 7_exception2.py
+++ 7_exception2.py
@@ -14,16 +14,13 @@
 
 def get_summ(num_one, num_two):
   try:
-    num_one = input('Введите число 1: ')
-    num_two = input('Введите число 2: ')
-    print(int(num_one)+int(num_two))
+    return(int(num_one)+int(num_two))
   except ValueError:
     print('нужно ввести число, попробуй снова')
-   
-    
+  return None   
 if __name__ == "__main__":
-    print(get_summ(2, 2))
-    print(get_summ(3, "3"))
-    print(get_summ("4", "4"))
-    print(get_summ("five", 5))
-    print(get_summ("six", "шесть"))
+  print(get_summ(2, 2))
+  print(get_summ(3, "3"))
+  print(get_summ("4", "4"))
+  print(get_summ("five", 5))
+  print(get_summ("six", "шесть"))
